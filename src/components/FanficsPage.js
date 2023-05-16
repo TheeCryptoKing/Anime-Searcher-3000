0f6e24333df77fb32ddc6938fee15d9a576a16ec
Mike Urezzio <mikeurezzio@gmail.com>
2023-05-16 01:32:51 -0400

    router basics

---
+++ src/components/FanficsPage.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+
+function FanficsPage () {
+    return (
+        <div className="FanficPage">
+            <header className="FanficPage-header">
+                <Navbar />
+            </header>
+            <div className="FanficPage-body">
+                <h1>Fanfiction page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default FanficsPage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 import "../stylesheets/Navbar.css";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  
+  const links = ["Home", "Fanfics", "Favorites"]
+
+  const anchors = links.map((link) => (
+    <NavLink to={"/" + link}>{link}</NavLink> 
+  ));
+
+  
+
   return (
-    <h1 className="page-title">Anime Searcher 3000</h1>
+    <div className="Nav-header">
+      <h1 className="page-title">Anime Searcher 3000</h1>
+      <nav>{anchors}</nav>
+    </div>
   )
 }
 

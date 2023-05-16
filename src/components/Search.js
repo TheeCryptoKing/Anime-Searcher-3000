04ee495c1875f8a77fc8ea90474c122af0582b88
Mike Urezzio <mikeurezzio@gmail.com>
2023-05-16 11:17:46 -0400

    added search route and basic component

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+
+function Search () {
+    return (
+        <div className="SearchPage">
+            <header className="SearchPage-header">
+                <Navbar />
+            </header>
+            <div className="SearchPage-body">
+                <h1>Search Page</h1>
+            </div>
+        </div>
+    )
+}
+
+
+export default Search;

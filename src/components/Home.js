eff14becbde650e21e666c2cf2b4640bc559cfbe
Eli Dawson <edawson@alumni.berklee.edu>
2023-05-16 17:13:21 -0400

    Home page arrow functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,8 @@
 import "../stylesheets/Home.css";
-import AnimeCard from "./AnimeCard";
+import CardContainer from "./CardConatiner";
 import Navbar from "./Navbar";
 
 function Home({ topAnimes, newAnimes }) {
-  const topAnimesArray = topAnimes.map((anime) => {
-    return (
-      <AnimeCard
-        key={anime.title}
-        image={anime.images.jpg.image_url}
-        name={anime.title}
-      />
-    );
-  });
-
-  const newAnimesArray = newAnimes.map((anime) => {
-    return (
-      <AnimeCard
-        key={anime.title}
-        image={anime.images.jpg.image_url}
-        name={anime.title}
-      />
-    );
-  });
 
   return (
     <div>
@@ -31,11 +12,11 @@ function Home({ topAnimes, newAnimes }) {
       <div className="home-body">
         <div className="top-animes">
           <h3 className="title">Top 10 Animes by Popularity:</h3>
-          <div className="card-container">{topAnimesArray}</div>
+          <CardContainer animeList={topAnimes}/>
         </div>
         <div className="new-animes">
           <h3 className="title">Top 10 Newest Animes:</h3>
-          <div className="card-container">{newAnimesArray}</div>
+          <CardContainer animeList={newAnimes}/>
         </div>
       </div>
     </div>

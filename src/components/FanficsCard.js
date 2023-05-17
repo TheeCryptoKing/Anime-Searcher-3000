b236f8a9471ae9d6616fcab4c16f4ec35ba9f61a
Eli Dawson <edawson@alumni.berklee.edu>
2023-05-17 11:45:06 -0400

    add fanfics functionality with post and get from json

--- src/components/FanficsCard.js
+++ src/components/FanficsCard.js
@@ -3,7 +3,7 @@ function FanficsCard ({ title, creator, image, animeName, genre, body}) {
         <article>
             <h2>{title}</h2>
             <h2>{creator}</h2>
-            <image src={image} alt={animeName}></image>
+            <img src={image} alt={animeName}></img>
             <h4>{animeName}</h4>
             <h4>{genre}</h4>
             <p>{body}</p>

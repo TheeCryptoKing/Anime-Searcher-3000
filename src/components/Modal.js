f6f3332c8c8104d288d09011683ff1101a071cb7
Ashay Morwale <ashay123.m@gmail.com>
2023-05-17 20:56:36 -0400

    changed modals and slight QOF imporvements

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,30 +11,38 @@ function Modal({ modalStuff, show, onClose }) {
   return (
     <div className="modal">
       <div className="modal-content">
-        <div className="modal-header">{modalStuff.title}</div>
-        <div className="modal-body">
-          <img
-            src={modalStuff.images.jpg.image_url}
-            alt={modalStuff.title}
-          ></img>
-          <p>Information:</p>
-          <p>rating: {modalStuff.rating}</p>
-          <p>score: {modalStuff.score}</p>
-          <p>rank: {modalStuff.rank}</p>
-          <p>episodes: {modalStuff.episodes}</p>
-          <p>status: {modalStuff.status}</p>
-          <p>release year: {modalStuff.year}</p>
-          <p>studios: </p>
-          {modalStuff.studios.map((studio) => (
-            <p key={studio.name}>{studio.name}</p>
-          ))}
-          {modalStuff.trailer.embed_url ? (
-            <iframe title={modalStuff.title} src={modalStuff.trailer.embed_url} />
-          ) : null}
-        </div>
-        <button onClick={onClose} className="button">
-          Close
+        <div className="modal-header">
+          {modalStuff.title}
+          <button onClick={onClose} className="button">
+          X
         </button>
+        </div>
+        <div className="modal-body">
+          <div className="row">
+            <img className="pt1"
+              src={modalStuff.images.jpg.image_url}
+              alt={modalStuff.title}
+            ></img>
+            <div className="pt1">
+              <p>rating: {modalStuff.rating}</p>
+              <p>score: {modalStuff.score}</p>
+              <p>rank: {modalStuff.rank}</p>
+            </div>
+          </div>
+            <div className="pt1">
+              <p>Information:</p>
+              <p>episodes: {modalStuff.episodes}</p>
+              <p>status: {modalStuff.status}</p>
+              <p>release year: {modalStuff.year}</p>
+              <p>studios: </p>
+              {modalStuff.studios.map((studio) => (
+                <p key={studio.name}>{studio.name}</p>
+              ))}
+            </div>
+            {modalStuff.trailer.embed_url ? (
+              <iframe width="60%" height="200px" className="pt1" title={modalStuff.title} src={modalStuff.trailer.embed_url} />
+            ) : "Nothing to show here..."}
+        </div>
       </div>
     </div>
   );

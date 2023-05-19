fdd94e6dcb293e7d9e1fe2a2c5d310dad28af195
Ashay Morwale <ashay123.m@gmail.com>
2023-05-18 23:53:40 -0400

    evans stuff

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,69 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../stylesheets/index.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "../stylesheets/index.css";
 import "../stylesheets/Modal.css";
-
 function Modal({ modalStuff, show, onClose }) {
   if (!show) {
     return null;
   }
-
-
   return (
     <div className="modal">
-      
       <div className="modal-content">
-        
         <div className="modal-header">
-          <h4>{modalStuff.title}</h4>
-          <button onClick={onClose} className="button">
-            <Link to="/Fanfics" state={modalStuff}>Write a Fanfic</Link>
-          </button>
+          {modalStuff.title}
           <button onClick={onClose} className="button">
             X
           </button>
+          <button onClick={onClose} className="button">
+            <Link to="/Fanfics" state={modalStuff}>Write a Fanfic</Link>
+          </button>
         </div>
-
         <div className="modal-body">
-            
-            <div className='row'>
-            <div className="pt1" >
-                {modalStuff.trailer.embed_url ? (
-                  <iframe classallow="fullscreen" width="700vw" height="393vw" title={modalStuff.title} src={modalStuff.trailer.embed_url} />
-                ) : "Nothing to show here..."}        
-              </div>    
-
+          <div className="row video-row">
+            <div className="col-sm-12" id="video">
+              {modalStuff.trailer.embed_url ? (
+                <div className="video-container">
+                  <iframe
+                    allow="fullscreen"
+                    width="100%"
+                    height="100%"
+                    className="pt1"
+                    title={modalStuff.title}
+                    src={modalStuff.trailer.embed_url}
+                  />
+                </div>
+              ) : (
+                "Nothing to show here..."
+              )}
             </div>
-
-            <div className='row'>
-              <div className="pt1">
-                <p>Information:</p>
-                <p>episodes: {modalStuff.episodes}</p>
-                <p>status: {modalStuff.status}</p>
-                <p>release year: {modalStuff.year}</p>
-                <p>studios: </p>
-                {modalStuff.studios.map((studio) => (
-                  <p key={studio.name}>{studio.name}</p>                 
-                ))}
-              </div>  
-              <img className="pt1"
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-sm-6 col-md-3" id="details">
+              <p>rating: {modalStuff.rating}</p>
+              <p>score: {modalStuff.score}</p>
+              <p>rank: {modalStuff.rank}</p>
+            </div>
+            <div className="col-sm-6 col-md-3">
+              <img
+                className="pt1"
                 src={modalStuff.images.jpg.image_url}
                 alt={modalStuff.title}
-              ></img>
-              <div className="pt1">
-                <p>rating: {modalStuff.rating}</p>
-                <p>score: {modalStuff.score}</p>
-                <p>rank: {modalStuff.rank}</p>
-              </div>
-
-            </div>   
-
-
-
+              />
+            </div>
+            <div className="offset-md-1 col-sm-6 col-md-3" id="information">
+              <p>Information:</p>
+              <p>episodes: {modalStuff.episodes}</p>
+              <p>status: {modalStuff.status}</p>
+              <p>release year: {modalStuff.year}</p>
+              <p>studios:</p>
+            </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
-
 export default Modal;
